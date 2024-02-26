956aa1e2f31b44875844defd6b37a2d19797fd78
Cyduart <futuristcgamez@gmail.com>
2024-02-26 15:09:14 -0300

    Beginning of the creation of the product repository.

--- src/main/java/com/example/CrudAPI/repositories/ProductRepository.java
+++ src/main/java/com/example/CrudAPI/repositories/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.example.CrudAPI.repositories;
 
+import com.example.CrudAPI.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ProductRepository  {
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
 }
