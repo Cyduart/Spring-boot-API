941bd817f8bdf972a481b2596e4b76e61faaf618
Cyduart <futuristcgamez@gmail.com>
2024-02-26 15:11:36 -0300

    Added the model of the product that will be manipulated

---
+++ src/main/java/com/example/CrudAPI/dtos/ProductRecordsDto.java
@@ -0,0 +1,10 @@
+package com.example.CrudAPI.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordsDto(@NotBlank String name, @NotNull BigDecimal value) {
+
+}
